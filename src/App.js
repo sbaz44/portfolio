f12c1ecc941c703fe96eb5ad6c979b0e5aae2bc1
Shahbaz Shaikh <shahbaz.shaikh@devomark.com>
2021-06-09 15:30:18 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import About from "./About";
 import Portfolio from "./Portfolio";
 import "./master.scss";
 
 function App() {
   return (
-    <BrowserRouter>
-      {/* MENU */}
-      {/* <nav className="col-12">
-            <Link className="btn gsap-btn mr-2" to="/">
-              Home
-            </Link>
-            <Link className="btn gsap-btn mr-2" to="/services">
-              Services
-            </Link>
-          </nav> */}
-
-      {/* CONTENT */}
+    <HashRouter>
       <Route path="/" exact>
         <Portfolio />
-        {/* {({ match }) => <Portfolio show={match !== null} />} */}
       </Route>
 
       <Route path="/about">
         <About />
-        {/* {({ match }) => <About show={match !== null} />} */}
       </Route>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
